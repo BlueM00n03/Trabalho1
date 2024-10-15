08c01bf92b1df9cd75cb23463b3ba806371bdec1
Sergio <sergio.alves@ua.pt>
2024-10-15 21:51:10 +0100

    So far

--- backup_files.sh
+++ backup_files.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 function backup_files() {
-    if [ -d "$1" ]; then
-    if [ ! -d "$2" ]; then
+    if [[ -d "$1" ]]; then
+    if [[ ! -d "$2" ]]; then
         mkdir $2
         echo "The destiny directory didn't exist but was created."
     fi
@@ -17,15 +17,29 @@ function backup_files() {
         case "${flag}" in
         c) c_flag='true' ;;
         *) echo "Invalid option: -${OPTARG}" >&2; exit 1 ;;
-    esac
+        esac
     done
-    for x in "$working_dir"/*; do
-        if [ -f "$x" ];then
-            date $x; 
-            echo $x
-        fi
+    for file in "$working_dir"/*; do
+        if [ -f "$file" ];then
+            filename=$(basename "$file")
+            found_flag=false
+            for file2 in "$target_dir"/*; do
+                file2_name=$(basename "$file2")
+                if [[ "$filename" == "$file2_name" ]]; then
+                    found_flag=true
+                    if [[ $(date -r $file +%s) -gt $(date -r $file2 +%s) ]];then
+                        echo "cp -a ""$file" "$file2 12345"
+                        cp -a "$file" "$file2"
+                    fi
+                fi
+            
+            done
+            if [[ $found_flag == "false" ]];then
+                echo "cp -a $file" "$target_dir/""$filename"
+                cp -a "$file" "$target_dir/$filename"
+            fi
 
-    
-done
+        fi
+    done
 }
-backup_files "$1"
+backup_files "$1" "$2"
